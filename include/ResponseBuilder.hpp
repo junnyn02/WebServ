54e1352d38652f762370c374b959cf93c47295a8
Julien Nguyen <junguyen@f1br9s5.clusters.42paris.fr>
2025-09-29 16:20:41 +0200

    begin response header for post

--- include/ResponseBuilder.hpp
+++ include/ResponseBuilder.hpp
@@ -2,6 +2,7 @@
 
 #include "utils.hpp"
 #include "Request.hpp"
+#include "serverCore.hpp"
 
 class ResponseBuilder
 {
@@ -10,17 +11,20 @@ class ResponseBuilder
         Request                             _request;
         std::map<std::string, std::string>  _mime;
         std::string                         _type;
+        std::string                         _body;
 
     public:
         ResponseBuilder(const Request &);
         ~ResponseBuilder(void);
         
-        const std::string   exec(void);
         void                tryGet(void);
         void                tryPost(void);
         void                isDir(const std::string &);
         void                isFile(const std::string &);
         
+        bool    checkMime(void) const;        
+
+        const std::string   sendResponse(void);
         const std::string   getCode(void) const;
         const std::string   getDate(void) const;
         const std::string   getType(void) const;
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -74,7 +74,7 @@ int	main(int, char**)
 		Request fresh(data);
 		ResponseBuilder	response(fresh);
 		fresh.printRequest(); // remove later
-		std::string hello = response.exec();
+		std::string hello = response.sendResponse();
 		for (size_t i = 0; i < hello.length(); i++)
 			data.buffer[i] = hello.c_str()[i];
 		for (size_t i = hello.length(); i < BUFFER_SIZE; i++)
