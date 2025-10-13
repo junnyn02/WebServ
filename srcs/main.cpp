b79aea0a1a9659824775f3138f1ca5336d8e5e67
bmouhib <bmouhib@student.42.fr>
2025-10-13 19:38:07 +0200

    Remodelling to allow for several servers

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -4,9 +4,9 @@
 #include "ResponseBuilder.hpp"
 
 // Signal handler function
-void signalHandler(int sig) {
-    std::cout << "Interrupt handle " << sig << std::endl;
-
+void signalHandler(int sig) 
+{
+    std::cout << "\nServer closed manually." << std::endl;
     exit(sig);
 }
 
@@ -18,10 +18,11 @@ int	main(int ac, char** av)
 
 	if (ac > 2)
 	{
-		std::cerr << "" << std::endl;
+		std::cerr << "Too many arguments./nFormat : ./webserv [config file](optional)]" << std::endl;
 		return (1);
 	}
 
+	// only if config file provided, else : should use default config file (or default config hard coded ?)
 	try
 	{
 		Config	conf(av[1]);
@@ -30,6 +31,8 @@ int	main(int ac, char** av)
 		std::cout << BOLD GREEN"[CONFIG PORT]: " RESET << std::endl;
 		for (size_t i = 0; i < servers.size(); i++)
 			std::cout << "\t" << servers[i].getPort() << std::endl;
+		
+		serverCore serv(servers);
 		return (0);
 	}
 	catch(const std::exception& e)
@@ -38,53 +41,51 @@ int	main(int ac, char** av)
 		return (1);
 	}
 	
+	// serverCore serv;
+	// serv.startServer();
 
-	serverCore serv;
-	ac == 1 ? serv.startServer() : serv.startServer(av[1]); // should add argv[1] : config file (-> parsing)
+	// struct epoll_event events[MAX_EVENTS];
 
-	struct epoll_event events[MAX_EVENTS];
-	// map with client fd / client data
+	// while (1)
+	// {
+	// 	int n_events = epoll_wait(serv._epoll_fd, events, MAX_EVENTS, 5000); // SET TIMEOUT (instead of -1)
+	// 	if (n_events == -1)
+	// 	{
+	// 		std::cerr << "Error: epoll_wait" << std::endl;
+	// 		break;
+	// 	}
 
-	while (1)
-	{
-		int n_events = epoll_wait(serv.epoll_fd, events, MAX_EVENTS, 5000); // SET TIMEOUT (instead of -1)
-		if (n_events == -1)
-		{
-			std::cerr << "Error: epoll_wait" << std::endl;
-			break;
-		}
-
-		for (int i = 0; i < n_events; i++) 
-		{
-			int client = events[i].data.fd;
-			// Event handling logic
-			if (client == serv.getfd()) // New client connection
-				serv.acceptNewClients();
-			else if (events[i].events & EPOLLIN)// Data received from an existing client
-			{
-				// int status = 
-				serv.receiveRequest(client); 
+	// 	for (int i = 0; i < n_events; i++) 
+	// 	{
+	// 		int client = events[i].data.fd;
+	// 		// Event handling logic
+	// 		if (client == serv.getfd()) // New client connection
+	// 			serv.acceptNewClients();
+	// 		else if (events[i].events & EPOLLIN)// Data received from an existing client
+	// 		{
+	// 			// int status = 
+	// 			serv.receiveRequest(client); 
 	
-				// if (status < 0)
-				// {
-				// 	std::cerr << "Error : request not received\n";
-				// 	serv.discussions.erase(client);
-				// }
-				/* else status == 0 -> request incomplete still receiving */
-			}
-			else if (events[i].events & EPOLLOUT)// Data received from an existing client
-			{
-				// serv.discussions[client].request.printRequest();
-				if (!serv.discussions[client].sendingResponse)
-				{
-					ResponseBuilder	response(serv.discussions[client].request);
-					std::string	resp = response.getHeader();
-					serv.setResponse(client, resp, resp.size());
-					serv.discussions[client].sendingResponse = true;
-				}
-				serv.sendResponse(client);
-			}
-		}
-	}
+	// 			// if (status < 0)
+	// 			// {
+	// 			// 	std::cerr << "Error : request not received\n";
+	// 			// 	serv._clients.erase(client);
+	// 			// }
+	// 			/* else status == 0 -> request incomplete still receiving */
+	// 		}
+	// 		else if (events[i].events & EPOLLOUT)// Data received from an existing client
+	// 		{
+	// 			// serv._clients[client].request.printRequest();
+	// 			if (!serv._clients[client].sendingResponse)
+	// 			{
+	// 				ResponseBuilder	response(serv._clients[client].request);
+	// 				std::string	resp = response.getHeader();
+	// 				serv.setResponse(client, resp, resp.size());
+	// 				serv._clients[client].sendingResponse = true;
+	// 			}
+	// 			serv.sendResponse(client);
+	// 		}
+	// 	}
+	// }
 	return (0);
 }
